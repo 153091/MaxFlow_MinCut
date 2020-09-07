cee864862ff85115d4c701bdc64311649026693f
153091 <xpoint06@gmail.com>
2020-09-07 22:33:02 +0300

    FlowNetwork start

---
+++ FlowNetwork.java
@@ -0,0 +1,12 @@
+import java.util.Queue;
+
+public class FlowNetwork {
+    private final int V;
+    private int E;
+    private Queue<FlowEdge>[] adj;
+
+    // create empty graph with V vertices
+    public FlowNetwork(int V) {}
+
+    /
+}
